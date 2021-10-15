e2d0360b42e8ced3f7910e67e81d22a7f18618a9
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-16 08:09:52 +0900

    use VFC in Todo.tsx

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,7 +1,8 @@
 import { TodoType } from "./types/todo";
+import { VFC } from "react";
 
 // export const Todo = (props: Pick<TodoType, "userId" | "title" | "completed">) => {
-export const Todo = (props: Omit<TodoType, "id">) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   const {title, userId, completed} = props;
   const completedMark = completed ? "[完]" : "[未]" ;
   return (
