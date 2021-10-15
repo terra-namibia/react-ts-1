5c9a4e535697463d0fc2a9519e478e40febac220
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-15 11:36:22 +0900

    init

--- src/Page1.tsx
+++ src/Page1.tsx
@@ -1,17 +1,27 @@
 export const Page1 = () => {
 
-  const onclickAction = () => calcTotal(1000);
+  const onclickAction1 = () => calcTotal(1000);
+  const onclickAction2 = () => console.log(getTotal(1000));
 
   // const calcTotal = (num) => {
-  const calcTotal = (num: number) => {
-    const total = num * 1.1;
-    console.log(total);
+  // const calcTotal = (num: number) => {
+  const calcTotal = (num: number): void => {
+    const total1 = num * 1.1;
+    console.log(total1);
+  }
+
+  const getTotal = (num: number): number => {
+    let total2: number = 0; 
+    total2 = num * 1.1;
+    return total2;
   }
 
   return (
     <div>
       <p>タイトル</p>
-      <button onClick={onclickAction}>ボタン実行</button>
+      <button onClick={onclickAction1}>ボタン1実行</button>
+      <br />
+      <button onClick={onclickAction2}>ボタン2実行</button>
     </div>
   )
 }
