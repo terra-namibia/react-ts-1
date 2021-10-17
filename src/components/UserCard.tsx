7c9b6e8457417f5c8df71473b49bd33e82c53700
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-17 23:06:20 +0900

    add component UserCard

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,29 @@
+import { VFC } from "react";
+import { UserProfile } from "../types/userProfile";
+
+type Props = {
+  user: UserProfile;
+};
+
+export const UserCard: VFC<Props> = (props) => {
+  const { user } = props;
+
+  const style = {
+    border: "solid 1px #ccc",
+    borderRadius: "10px",
+    padding: "0 15px",
+    margin: "8px"
+  };
+  return (
+    <div style={style}>
+      <dl>
+        <dt>name</dt>
+        <dd>{user.name}</dd>
+        <dt>mail</dt>
+        <dd>{user.email}</dd>
+        <dt>address</dt>
+        <dd>{user.address}</dd>
+      </dl>
+    </div>
+  );
+};
